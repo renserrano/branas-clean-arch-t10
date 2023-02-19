db9c04b5d3bdc34da9c1ed91a5ba836eab159752
renserrano <renan_jau@yahoo.com.br>
2023-02-18 23:34:35 -0300

    Adicionando casos de uso Order, Cupom e Frete

---
+++ projeto-parte-3/src/application/usecase/GetOrder.ts
@@ -0,0 +1,26 @@
+import OrderRepository from "../../OrderRepository";
+import OrderRepositoryDatabase from "../../OrderRepositoryDatabase";
+
+export default class GetOrder {
+
+    constructor(
+        readonly orderRepository: OrderRepository = new OrderRepositoryDatabase()
+    ) {
+    }
+
+    async execute(id: string): Promise<Output> {
+        const order = await this.orderRepository.get(id);
+        const output: Output = {
+            code: order.getCode(),
+            total: order.getTotal(),
+            freight: order.freight
+        };
+        return output;
+    }
+}
+
+type Output = {
+    code: string,
+    total: number,
+    freight: number
+}
