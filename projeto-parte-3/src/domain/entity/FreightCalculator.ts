7adf0dc25f9548d1d984cfa90de23e61c31510fd
renserrano <renan_jau@yahoo.com.br>
2023-02-20 00:07:25 -0300

    Calculo do Volume pegando direto de Product

--- projeto-parte-3/src/domain/entity/FreightCalculator.ts
+++ projeto-parte-3/src/domain/entity/FreightCalculator.ts
@@ -2,7 +2,7 @@ import Product from "./Product";
 
 export default class FreightCalculator {
     static calculate (product: Product) {
-        const volume = ((product.width/100) * (product.height/100) * (product.length/100));
+        const volume = product.getVolume();
         const density = product.weight / volume;
         const itemFreight = 1000 * volume * (density/100);
         return itemFreight;
